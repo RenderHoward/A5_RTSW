cd61e9471390e737db759976d228b93993e23a43
RenderHoward <David.Howard@Ultrapoise.com>
2024-11-15 23:15:33 -0500

    Parameterized source url Prepares for future scaling to other/multiple json sources

--- RTSW.py
+++ RTSW.py
@@ -1,11 +1,10 @@
 import urllib.request, json
 import Persistance
 
+urlstr = "https://services.swpc.noaa.gov/products/geospace/propagated-solar-wind-1-hour.json"
 
-
-
-with urllib.request.urlopen("https://services.swpc.noaa.gov/products/geospace/propagated-solar-wind-1-hour.json") as url:
-    data = json.load(url)
+with urllib.request.urlopen(urlstr) as urlstr:
+    data = json.load(urlstr)
 
     store = Persistance.Store("SolarDB", "SolarWind")
 
