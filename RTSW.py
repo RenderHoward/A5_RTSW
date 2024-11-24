2640bef3e20c51ac663e41d99bfc4952e2ed3045
RenderHoward <David.Howard@Ultrapoise.com>
2024-11-23 22:31:46 -0500

    Using new time bounds to regulate which new records to push

--- RTSW.py
+++ RTSW.py
@@ -1,17 +1,40 @@
 import urllib.request, json
 import Persistence
+from pathlib import Path
+def main():
+    urlstr = "https://services.swpc.noaa.gov/products/geospace/propagated-solar-wind-1-hour.json"
 
     store = Persistence.Store("SolarDB", urlstr )
-urlstr = "https://services.swpc.noaa.gov/products/geospace/propagated-solar-wind-1-hour.json"
 
-with urllib.request.urlopen(urlstr) as urlstr:
-    data = json.load(urlstr)
+    with urllib.request.urlopen(urlstr) as url:
+        data = json.load(url)
 
+    store.addtable(urlstr, data)
 
-    store.begintrans()
+    pullnewest(store)
 
-    for row in data:
-        store.addrecord(row)
+    urlstr = "https://services.swpc.noaa.gov/products/solar-wind/plasma-3-day.json"
 
-    store.endtrans()
+    with urllib.request.urlopen(urlstr) as url:
+        data = json.load(url)
 
+    store = Persistence.Store("SolarDB", urlstr)
+
+    store.addtable( urlstr, data )
+
+def pullnewest(storage):
+    global data
+    storage.refreshtimebracket()
+
+    with urllib.request.urlopen(storage.URL) as url:
+        data = json.load(url)
+
+    storage.begintrans()
+
+    for row in data[1:] :
+        if row[0] > storage.latest :
+            storage.addrecord(row)
+
+    storage.endtrans()
+
+main()
